94cb8931ea594dd077fb8eeeba89e8588d1d5622
nishikori <rxk12ki04yo@gmail.com>
2021-06-23 11:23:16 +0900

    消した

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 
 Rails.application.routes.draw do
   get 'pictures/studenthome'
-  root '#index'   #トップページを○○コントローラのindexアクションに設定
   get 'pictures/se_namecheck'
   get 'students/sample1'
   resources :details
