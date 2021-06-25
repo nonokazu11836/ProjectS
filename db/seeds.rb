cadee8403deb744cfe558f91d2a5414404028036
torikizoku334 <kamasenoouji@gmail.com>
2021-06-25 10:11:08 +0900

    seeds書き込み完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create(name:"‰ª —é“Ş",    login_id:'201001',pass:'icc-201001',gakka_id:1);
+Student.create(name:"K“c t“Ş",  login_id:'201002',pass:'icc-201002',gakka_id:1);
+Student.create(name:"Œ´ ‚Ì‚Ì‚©",  login_id:'201003',pass:'icc-201003',gakka_id:1);
+Student.create(name:"×–Ø x",    login_id:'201004',pass:'icc-201004',gakka_id:1);
+
+Student.create(name:"ó“c —[‹H”T",login_id:'202001',pass:'icc-202001',gakka_id:2);
+Student.create(name:"ˆÀ’B —I–ç",@login_id:'202003',pass:'icc-202003',gakka_id:2);
+Student.create(name:"…Œ´ —³”V‰î",login_id:'202004',pass:'icc-202004',gakka_id:2);
+Student.create(name:"‰eR ‹v–¾",@login_id:'202005',pass:'icc-202005',gakka_id:2);
+Student.create(name:"¬Œ´ ìX‰Ø",login_id:'202006',pass:'icc-202006',gakka_id:2);
+Student.create(name:"•iì Ÿ©l",@login_id:'202007',pass:'icc-202007',gakka_id:2);
+Student.create(name:"‰i‹g O‹G",  login_id:'202022',pass:'icc-202022',gakka_id:2);
+Student.create(name:"[“c —y‘å",  login_id:'202008',pass:'icc-202008',gakka_id:2);
+Student.create(name:"•Ÿ“‡ —Il",@login_id:'202009',pass:'icc-202009',gakka_id:2);
+Student.create(name:"•Ÿ“c —¯”ü",  login_id:'202010',pass:'icc-202010',gakka_id:2);
+Student.create(name:"–x“à q",    login_id:'202011',pass:'icc-202011',gakka_id:2);
+Student.create(name:"‘ºã •–‰À",  login_id:'202012',pass:'icc-202012',gakka_id:2);
+
+Student.create(name:"ˆ¢H —´“ñ",  login_id:'202002',pass:'icc-202002',gakka_id:3);
+Student.create(name:"”Ñ’Ë —G‰î",  login_id:'202013',pass:'icc-202013',gakka_id:3);
+Student.create(name:"”Â‘q Œ[‘¾",  login_id:'202014',pass:'icc-202014',gakka_id:3);
+Student.create(name:"‘åX çŒd",  login_id:'202015',pass:'icc-202015',gakka_id:3);
+Student.create(name:"—‡ “O",    login_id:'202016',pass:'icc-202016',gakka_id:3);
+Student.create(name:"ë–ì Œ[‘¾",  login_id:'202017',pass:'icc-202017',gakka_id:3);
+Student.create(name:"™–{ ”ü",  login_id:'202018',pass:'icc-202018',gakka_id:3);
+Student.create(name:"{“c ˆê‹P",  login_id:'202019',pass:'icc-202019',gakka_id:3);
+Student.create(name:"{“c Œ’‘¾",  login_id:'202020',pass:'icc-202020',gakka_id:3);
+Student.create(name:"’·“‡ —Bl",  login_id:'202021',pass:'icc-202021',gakka_id:3);
+Student.create(name:"‹ÑD ´”ü",  login_id:'202023',pass:'icc-202022',gakka_id:3);
+Student.create(name:"–ìX‘º ˜a÷",login_id:'202024',pass:'icc-202023',gakka_id:3);
+Student.create(name:"••” —El",@login_id:'202025',pass:'icc-202014',gakka_id:3);
+Student.create(name:"Œ´ ‰põ",    login_id:'202026',pass:'icc-202015',gakka_id:3);
+Student.create(name:"ê Œ´ ”¹l",  login_id:'202027',pass:'icc-202026',gakka_id:3);
+Student.create(name:"O“‡ qS",@login_id:'202028',pass:'icc-202027',gakka_id:3);
+Student.create(name:"O’J ¢",@login_id:'202029',pass:'icc-202028',gakka_id:3);
