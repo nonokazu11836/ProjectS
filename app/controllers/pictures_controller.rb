b4b72e9fa35c35b649034f7cdf639b408051de64
H-8810 <hedgehog0505@gmail.com>
2021-06-29 13:22:26 +0900

    update search

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -63,7 +63,11 @@ class PicturesController < ApplicationController
 
   # 検索機能
   def search
-
+    @pictures = Array.new
+    @pictures = Picture.all
+    if request.post?
+      @pictures = Picture.where(:detail.student_id == params[:student_id],params[:event_id])
+    end
   end
 
   def it_member
