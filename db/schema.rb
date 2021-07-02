6624d4a55208ee8ac661bfdd41cd3b7c1f31f322
nishikori <rxk12ki04yo@gmail.com>
2021-07-02 18:41:13 +0900

    devise導入。新規登録時、学生番号のカラムを追加。登録も確認。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_052158) do
+ActiveRecord::Schema.define(version: 2021_07_02_091217) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -59,6 +59,19 @@ ActiveRecord::Schema.define(version: 2021_06_22_052158) do
     t.integer "gakka_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "student_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
