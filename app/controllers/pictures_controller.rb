8747f372140aac5a30236598168cc7ee0c2b6b75
nishikori <rxk12ki04yo@gmail.com>
2021-07-03 12:47:21 +0900

    pictures_controllerにallupメソッドを追加。view/picturesにallup.html.erbを追加。

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -105,6 +105,10 @@ class PicturesController < ApplicationController
     @pictures = Picture.where(student_id: student.id)
   end
 
+  def allup
+
+  end
+
   private
      #Use callbacks to share common setup or constraints between actions.
     def set_picture
