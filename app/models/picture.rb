ecaaae3e76725f3c50f9b447d0bed285950cc57f
nishikori <rxk12ki04yo@gmail.com>
2021-07-15 14:53:20 +0900

    picture.rbにバリデーションを設定。写真を選択していないとエラーが出るように変更。

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,5 +2,6 @@ class Picture < ApplicationRecord
   mount_uploader :place, AvatarUploader  #写真アップ用
     has_one :avatar
     accepts_nested_attributes_for :avatar, allow_destroy: true
+    validates :place, presence: true
     
 end
